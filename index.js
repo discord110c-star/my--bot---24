61864b15bee89a5c322a91050288a9189db1e409
discord110c-star <discord110c@gmail.com>
2026-04-02 16:53:24 +0200

    5index.js

--- index.js
+++ index.js
@@ -4,22 +4,9 @@ const bot = mineflayer.createBot({
     host: 'blackfish.aternos.host', 
     port: 23397,
     username: 'BodyGuard', 
-    auth: 'offline',
-    checkTimeoutInterval: 90000,
-    hideErrors: true
+    auth: 'offline'
 });
 
-bot.on('spawn', () => {
-    console.log('BOT IS IN THE SERVER NOW');
-    setInterval(() => {
-        bot.swingArm('right');
-    }, 10000);
-});
-
-bot.on('error', (err) => {
-    console.log('Error:', err.code);
-});
-
-bot.on('end', () => {
-    console.log('Reconnecting...');
-});
+bot.on('spawn', () => console.log('Bot is IN!'));
+bot.on('error', (err) => console.log('Error:', err.message));
+bot.on('end', () => console.log('Disconnected'));
