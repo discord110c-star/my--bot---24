9a4af532068b14576b033401b3e2e835ba651601
discord110c-star <discord110c@gmail.com>
2026-04-02 15:32:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
 const mineflayer = require('mineflayer');
 
-function createBot() {
-    const bot = mineflayer.createBot({
-        host: 'SELVER02.aternos.me', 
-        port: 23397,
-        username: 'Guard_Bot',
-        version: '1.21.11'
-    });
+const bot = mineflayer.createBot({
+    host: 'SELVER02.aternos.me', 
+    port: 23397,
+    username: 'Guard_Bot',
+    version: '1.21.11'
+});
 
-    bot.on('spawn', () => {
-        console.log('Bot joined and is active');
-        
-        setInterval(() => {
-            bot.swingArm('right');
-            bot.setControlState('jump', true);
-            setTimeout(() => bot.setControlState('jump', false), 500);
-        }, 20000); 
-    });
+bot.on('spawn', () => {
+    console.log('Bot joined!');
+    setInterval(() => {
+        bot.swingArm('right');
+        bot.setControlState('jump', true);
+        setTimeout(() => bot.setControlState('jump', false), 500);
+    }, 20000); 
+});
 
-    bot.on('error', (err) => console.log(err));
-    bot.on('end', () => {
-        console.log('Disconnected, reconnecting...');
-        setTimeout(createBot, 5000);
-    });
-}
-
-createBot();
+bot.on('error', (err) => console.log(err));
+bot.on('end', () => console.log('Disconnected'));
